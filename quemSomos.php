62f31c4bbab867aa579c9c8612a798b84ed55f79
brucesilva <bruce_silva1010@Hotmail.com>
2022-03-25 14:15:03 -0300

    Tela cadastro e index feita em novo modelo

---
+++ quemSomos.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" href="assets/css/cssBootstrap.css">
+
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container login-container">
+        <div class="row">
+            <div class="col-md-6 login-form-1">
+                <h1>Quem Somos </h1>
+
+                <p style="color: white;">
+
+                    Somos uma empresa que acreditamos no potencial ilimintado das pessoas
+                </p>
+
+                <p style="color: white;">
+                    Nossa empresa trabalha diariamente para melhorar os serviços para os nossos usuários
+                    com um time altamente capacitados.
+                </p>
+
+                <p style="color: white;">
+                    Venha fazer parte desta empresa que cresce cada dia mais.
+                </p>
+
+                <p style="color: white;">
+                    Se você tem um sonho, acredite que ele pode ser realizado.
+                </p>
+
+            </div>
+            <div class="col-md-6 login-form-2">
+                <div class="login-logo">
+                    <img src="assets/img/icon100.png" alt="" />
+                </div>
+
+                <!-- aqui pra baixo já começa o outro login -->
+                <h1>Cadastro</h1>
+
+                <form action="">
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" required placeholder="Your Name *" value="" />
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" class="form-control" required placeholder="Your Email *" value="" />
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" class="form-control" required placeholder="Your Password *" value="" />
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" class="btn btnSubmit" value="Login" />
+                    </div>
+                    <div class="form-group">
+
+                        <a href="#" class="btnForgetPwd" value="Login">Esqueceu a Senha?</a>
+                    </div>
+                </form>
+
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
